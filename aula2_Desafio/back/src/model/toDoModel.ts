4d77b392491b552463427c51b52a83ec24e4f329
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-03-03 14:31:53 -0300

    commit

--- aula2_Desafio/back/src/model/toDoModel.ts
+++ aula2_Desafio/back/src/model/toDoModel.ts
@@ -1,24 +1,69 @@
-import express, {Request, Response, Router} from 'express'
-import mongoose, { Schema, Document } from 'mongoose';
+import * as React from 'react';
+import { styled } from '@mui/joy/styles';
+import Input from '@mui/joy/Input';
+import CheckCircleOutlined from '@mui/icons-material/CheckCircleOutlined';
 
-const router : Router = express.Router();
-
-interface IList extends Document {
-    title: string;
-    description: string[];
-    completed: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-const listSchema: Schema = new Schema({
-    title : { type: String, required: true },
-    description: { type: [String], required: true },
-    completed: { type: String, required: true },
-    createdAt: { type: Date, required: true },
-    updatedAt: { type: Date, required: true },
+const StyledInput = styled('input')({
+  border: 'none',
+  minWidth: 0,
+  outline: 0,
+  padding: 0,
+  paddingTop: '1em',
+  flex: 1,
+  color: 'inherit',
+  backgroundColor: 'transparent',
+  fontFamily: 'inherit',
+  fontSize: 'inherit',
+  fontStyle: 'inherit',
+  fontWeight: 'inherit',
+  lineHeight: 'inherit',
+  textOverflow: 'ellipsis',
+  '&::placeholder': {
+    opacity: 0,
+    transition: '0.1s ease-out',
+  },
+  '&:focus::placeholder': {
+    opacity: 1,
+  },
+  '&:focus ~ label, &:not(:placeholder-shown) ~ label, &:-webkit-autofill ~ label': {
+    top: '0.5rem',
+    fontSize: '0.75rem',
+  },
+  '&:focus ~ label': {
+    color: 'var(--Input-focusedHighlight)',
+  },
+  '&:-webkit-autofill': {
+    alignSelf: 'stretch',
+  },
 });
 
-const List = mongoose.model<IList>('List', listSchema);
+const StyledLabel = styled('label')(({ theme }) => ({
+  position: 'absolute',
+  lineHeight: 1,
+  top: 'calc((var(--Input-minHeight) - 1em) / 2)',
+  color: theme.vars.palette.text.tertiary,
+  fontWeight: theme.vars.fontWeight.md,
+  transition: 'all 150ms cubic-bezier(0.4, 0, 0.2, 1)',
+}));
 
-export default List ;
+const InnerInput = React.forwardRef(function InnerInput(props : any, ref) {
+  const id = React.useId();
+  return (
+    <React.Fragment>
+      <StyledInput {...props} ref={ref} id={id} />
+      <StyledLabel htmlFor={id}>Label</StyledLabel>
+    </React.Fragment>
+  );
+});
+
+export default function FloatingLabelInput() {
+  return (
+    <div>
+      <StyledInput
+        placeholder="A placeholder"
+        type="password"
+      />
+      <CheckCircleOutlined />
+    </div>
+  );
+}
--- aula2_Desafio/front/src/app/home/page.tsx
+++ aula2_Desafio/front/src/app/home/page.tsx
@@ -3,15 +3,30 @@
 import Image from "next/image";
 import Drawer from '@mui/joy/Drawer';
 import ModalClose from '@mui/joy/ModalClose';
-import { DialogTitle } from "@mui/material";
+import { Checkbox, DialogTitle, FormControlLabel, FormGroup, Input, Modal, TextField } from "@mui/material";
 import { useState, useEffect } from "react";
 import settings from '../../../public/image/settings.png'
 import TaskCardList from "@/components/list";  
+import plus from '../../../public/image/plus.png'
+import { Box, ModalDialog, Typography } from "@mui/joy";
+import Sheet from '@mui/joy/Sheet';
+import { CheckCircleOutlined } from "@mui/icons-material";
 
 export default function Home() {
-  const [open, setOpen] = useState<boolean>(false);
-  const [data, setData] = useState<any[]>([]); // Usando um array vazio como valor inicial
 
+  // estados
+  const [open, setOpen] = useState<boolean>(false);
+  const [openAddModal, setOpenAddModal] = useState<boolean>(false);
+  const [data, setData] = useState<any[]>([]); 
+
+
+  const [inputs, setInputs] = useState<string[]>([]); // Definindo 
+  const handleAddInput = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      setInputs([...inputs, '']); // Adicionar uma nova entrada ao pressionar Enter
+    }
+
+  // get data
   useEffect(() => {
     fetch('http://localhost:8080/api/task')
       .then(response => response.json())  
@@ -21,6 +36,63 @@ export default function Home() {
 
   return (
     <>
+    {/* modal */}
+    {openAddModal &&(
+
+      <Modal
+      aria-labelledby="modal-title"
+      aria-describedby="modal-desc"
+      open={openAddModal}
+      onClose={() => setOpenAddModal(false)}
+      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+      <Sheet
+        variant="outlined"
+        sx={{ maxWidth: 500, borderRadius: 'md', p: 3, boxShadow: 'lg' }}
+      >
+      
+        {/* modal title */}
+        <Typography component="h2" id="modal-title" evel="h4" textColor="inherit" sx={{ fontWeight: 'lg', mb: 1 }}>
+        Adicionar tarefa
+        </Typography>
+
+        {/* campos */}
+
+        <TextField
+          label="Título"
+          id="standard-size-normal"
+          defaultValue=""
+          variant="standard"
+          color="secondary"
+        />
+
+        <h1 className="my-6" >Adicione tarefas abaixo</h1>
+        <FormGroup>
+
+        <FormGroup>
+        {inputs.map((input, index) => (
+          <div key={index} className="flex mb-2">
+            <FormControlLabel control={<Checkbox defaultChecked />} label="" />
+            <Input
+              placeholder="Digite a tarefa"
+              value={input}
+              onChange={(e) => {
+                const newInputs = [...inputs];
+                newInputs[index] = e.target.value;
+                setInputs(newInputs);
+              }}
+              onKeyDown={handleAddInput} 
+            />
+          </div>
+        ))}
+      </FormGroup>
+        </FormGroup>
+        
+      </Sheet>
+      </Modal>
+
+    )}
+    {/* main content */}
       <div className="flex h-screen gap-3 p-6 bg-gray-50">
         <div className="bg-gray-200 rounded-lg w-4/12 flex items-center flex-col">
           <div className="flex items-center">
@@ -38,10 +110,16 @@ export default function Home() {
         </div>
 
         <div className="bg-gray-300 rounded-lg w-full">
-          <h1 className="text-3xl font-extrabold m-6 text-black">20 de fevereiro de 2025</h1> 
+
+          <div className="w-full flex  items-center justify-between p-4" >
+            <h1 className="text-3xl font-extrabold m-6 text-black">20 de fevereiro de 2025</h1>
+            <Image className="cursor-pointer" width={50} height={50} alt="plus" src={plus} onClick={ event => {setOpenAddModal(true)}}></Image>
+          </div>
+
           <TaskCardList items={data} />
+          
         </div>
       </div>
     </>
   );
-}
+}}
